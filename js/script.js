91d297af567a577b36d6db85c61fa70edf5177a3
kevinmanobanda27 <andres000kmj@gmail.com>
2023-07-17 12:48:32 -0500

    arreglo pdf

--- js/script.js
+++ js/script.js
@@ -47,18 +47,104 @@ window.addEventListener("scroll", function() {
 
 
 function openPDF() {
-  var pdfURL = "/files/capone/RESUMEN CAPITULO I.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  var pdfURL = "/files/capone/RESUMEN CAPITULO I_2.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
   
   // Abre el PDF en una nueva ventana o pestaña
   window.open(pdfURL, "_blank");
 }
 
 function downloadPDF() {
-  var pdfURL = "/files/capone/RESUMEN CAPITULO I.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  var pdfURL = "/files/capone/RESUMEN CAPITULO I_2.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
   
   // Descarga el PDF
   var link = document.createElement("a");
   link.href = pdfURL;
   link.download = "prueba1.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.click();
+}
+
+function openPDF2() {
+  var pdfURL = "/files/captwo/CAPITULO2.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Abre el PDF en una nueva ventana o pestaña
+  window.open(pdfURL, "_blank");
+}
+
+function downloadPDF2() {
+  var pdfURL = "/files/capTWO/CAPITULO2.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Descarga el PDF
+  var link = document.createElement("a");
+  link.href = pdfURL;
+  link.download = "ARCHIVO2.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.click();
+}
+
+function openPDF3() {
+  var pdfURL = "/files/capthree/CAPITULO3.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Abre el PDF en una nueva ventana o pestaña
+  window.open(pdfURL, "_blank");
+}
+
+function downloadPDF3() {
+  var pdfURL = "/files/capthree/CAPITULO3.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Descarga el PDF
+  var link = document.createElement("a");
+  link.href = pdfURL;
+  link.download = "archivo3.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.click();
+}
+
+function openPDF4() {
+  var pdfURL = "/files/capfour/CAPITULO4.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Abre el PDF en una nueva ventana o pestaña
+  window.open(pdfURL, "_blank");
+}
+
+function downloadPDF4() {
+  var pdfURL = "/files/capfour/CAPITULO4.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Descarga el PDF
+  var link = document.createElement("a");
+  link.href = pdfURL;
+  link.download = "archivo4.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.click();
+}
+
+
+function openPDF5() {
+  var pdfURL = "/files/capfive/CAPITULO5.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Abre el PDF en una nueva ventana o pestaña
+  window.open(pdfURL, "_blank");
+}
+
+function downloadPDF5() {
+  var pdfURL = "/files/capfive/CAPITULO5.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Descarga el PDF
+  var link = document.createElement("a");
+  link.href = pdfURL;
+  link.download = "archivo5.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.click();
+}
+
+function openPDF6() {
+  var pdfURL = "/files/capfive/CAPITULO5.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Abre el PDF en una nueva ventana o pestaña
+  window.open(pdfURL, "_blank");
+}
+
+function downloadPDF6() {
+  var pdfURL = "/files/capfive/CAPITULO5.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  
+  // Descarga el PDF
+  var link = document.createElement("a");
+  link.href = pdfURL;
+  link.download = "archivo5.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
   link.click();
 }

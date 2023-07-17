af41e9c5a6b362397272386c3c09ef6443c3cea3
kevinmanobanda27 <andres000kmj@gmail.com>
2023-07-17 13:30:13 -0500

    comentario cambios

--- js/script.js
+++ js/script.js
@@ -140,12 +140,12 @@ function openPDF6() {
 }
 
 function downloadPDF6() {
-  var pdfURL = "/files/capfive/CAPITULO5.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
+  var pdfURL = "/files/capsix/CAPITULO6.pdf"; // Reemplaza con la ruta correcta de tu archivo PDF
   
   // Descarga el PDF
   var link = document.createElement("a");
   link.href = pdfURL;
-  link.download = "archivo5.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
+  link.download = "archivo6.pdf"; // Reemplaza con el nombre que desees para el archivo descargado
   link.click();
 }
 
@@ -174,3 +174,265 @@ function descargarArchivo() {
 
 // Asignar el evento click al botón para activar la descarga
 document.getElementById("descargarBoton").addEventListener("click", descargarArchivo);
+
+
+
+function descargarArchivo2() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "CAPITULO2.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBoton2").addEventListener("click", descargarArchivo2);
+
+
+
+function descargarArchivo3() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "CAPITULO3.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBoton3").addEventListener("click", descargarArchivo3);
+
+
+function descargarArchivo4() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "CAPITULO4.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBoton4").addEventListener("click", descargarArchivo4);
+
+
+function descargarArchivo5() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "CAPITULO5.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBoton5").addEventListener("click", descargarArchivo5);
+
+
+function descargarArchivo6() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "CAPITULO6.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBoton6").addEventListener("click", descargarArchivo6);
+
+function descargarArchivoExtra() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "GuiaPractica9.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBotonExtra").addEventListener("click", descargarArchivoExtra);
+
+
+
+function descargarArchivoExtra2() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "Guia11ROBasicaMonomodo.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBotonExtra2").addEventListener("click", descargarArchivoExtra2);
+
+
+function descargarArchivoExtra3() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "Guia12ROBasicaMultimodo.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBotonExtra3").addEventListener("click", descargarArchivoExtra3);
+
+
+function descargarArchivoExtra4() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "PRACTICA2MULTIPLEXACIONWDM2A1.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBotonExtra4").addEventListener("click", descargarArchivoExtra4);
+
+
+function descargarArchivoExtra5() {
+  // Contenido del archivo Word (puedes cambiar el contenido aquí)
+  const contenido = "¡Hola! Este es un archivo Word de ejemplo.";
+
+  // Crear un nuevo Blob con el contenido y el tipo de archivo
+  const blob = new Blob([contenido], { type: "application/msword" });
+
+  // Crear un enlace para descargar el archivo
+  const url = URL.createObjectURL(blob);
+  const enlaceDescarga = document.createElement("a");
+
+  enlaceDescarga.href = url;
+  enlaceDescarga.download = "Practica6.doc"; // Nombre del archivo Word
+
+  // Agregar el enlace al DOM y simular un clic para descargar el archivo
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+
+  // Eliminar el enlace después de la descarga
+  document.body.removeChild(enlaceDescarga);
+}
+
+// Asignar el evento click al botón para activar la descarga
+document.getElementById("descargarBotonExtra5").addEventListener("click", descargarArchivoExtra5);
